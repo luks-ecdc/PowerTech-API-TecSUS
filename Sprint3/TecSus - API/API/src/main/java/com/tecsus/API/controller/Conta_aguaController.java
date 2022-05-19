5fc40e00a678cf004c7f1a49c960b8d8256a0a76
LuccaSantDev <luccasantdev@gmail.com>
2022-05-19 10:15:44 -0300

    este eh o recommit

--- Sprint3/TecSus - API/API/src/main/java/com/tecsus/API/controller/Conta_aguaController.java
+++ Sprint3/TecSus - API/API/src/main/java/com/tecsus/API/controller/Conta_aguaController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tecsus.API.entities.Conta_agua;
+import com.tecsus.API.entities.Contrato;
 import com.tecsus.API.repository.Conta_aguaRepository;
+import com.tecsus.API.repository.ContratoRepository;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -30,6 +32,9 @@ public class Conta_aguaController {
 	@Autowired 
 	Conta_aguaRepository conta_aguaRepository;
 	
+	@Autowired 
+	ContratoRepository contratoRepository;
+	
 	@GetMapping("/Contas_agua") // metodo GET
 	public List<Conta_agua> getContas_agua(){
 		return conta_aguaRepository.findAll();
@@ -38,6 +43,11 @@ public class Conta_aguaController {
 	@GetMapping("/Conta_agua/{id_conta}") // metodo GET
 	public  Conta_agua getConta_aguaId(@PathVariable(value = "id_conta") long id){
 		return conta_aguaRepository.findById(id);
+	}
+	
+	@GetMapping("/Conta_agua_rgi/{rgi_fk}") // metodo GET
+	public  List<Conta_agua> getConta(@PathVariable(value = "rgi_fk") long rgi_fk){
+		return conta_aguaRepository.findByRgiAguaFk(rgi_fk);
 	}
 	
 
@@ -51,6 +61,19 @@ public class Conta_aguaController {
 		return conta_aguaRepository.save(conta_agua);
 	}
 	
+	
+	@PutMapping("/conta_agua/{rgi_agua}/contrato/{instalacao_cont}")
+	public Conta_agua ColocarContratoNaConta(
+	            @PathVariable Long rgi_agua,
+	            @PathVariable Long instalacao_cont
+	    ) {
+	    	 Conta_agua  conta_agua = conta_aguaRepository.findById(rgi_agua).get();
+	        Contrato contrato = contratoRepository.findById(instalacao_cont).get();
+	        conta_agua.setContrato(contrato);
+	        return conta_aguaRepository.save(conta_agua);
+	    }
+	
+
 	@DeleteMapping("/Conta_agua")
 	public  void deleteConta_agua(@RequestBody Conta_agua conta_agua) {
 		conta_aguaRepository.delete(conta_agua);
